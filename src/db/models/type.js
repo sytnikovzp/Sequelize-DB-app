5a7b83016b4cd231af5a87672a3c3d5d971178b7
alex <sytnikov.zp@gmail.com>
2024-07-09 23:47:17 +0300

    Generate model Type

---
+++ src/db/models/type.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Type extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Type.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Type',
+    }
+  );
+  return Type;
+};
