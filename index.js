bd6f7da74dec3c875e93fcd5b9836c9b3cc704b8
alex <sytnikov.zp@gmail.com>
2024-07-10 12:06:06 +0300

    Install dotenv

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Server is started!');
+
+const db = require('./src/db/models');
+
+const dbCheck = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log(`Connection with DB cars has been successfully done!`);
+  } catch (error) {
+    console.log(`Can't connect to DB: `, error.message);
+  }
+};
+
+dbCheck();
--- src/config/postgresConfig.js
+++ src/config/postgresConfig.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'root',
-    database: 'cars',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     migrationStorage: 'json',
     seederStorage: 'json',
